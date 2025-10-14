d67fb64cdf0db9f83234d24897386a44c9d7b3fe
Loopy <loopy@heyratel.com>
2025-10-14 17:01:44 +0900

    chore

--- SnakeGame/Sources/ContentView.swift
+++ SnakeGame/Sources/ContentView.swift
@@ -57,10 +57,6 @@ public struct ContentView: View {
                         height: size.height / model.grid)
                     context.fill(Rectangle().path(in: rect), with: .color(.red))
                 }
-
-                for food in model.foods {
-
-                }
             }
         }
         .task {
