acc3bb016ff3f7238de449f31fc37d031f3250f2
Loopy <loopy@heyratel.com>
2025-10-14 15:07:45 +0900

    add comment

--- SnakeGame/Sources/ContentViewModel.swift
+++ SnakeGame/Sources/ContentViewModel.swift
@@ -2,7 +2,7 @@ import Combine
 import Foundation
 
 public final class ContentViewModel: ObservableObject {
-
+    // Snake can go to x: grid - 1, y: grid - 1
     let grid: Double
 
     @Published var snake: [Square] = []
