3f88df0d723274bf3d53a217e4adfe6c5e2f28f9
신동규 <donggyu9410@gmail.com>
2025-10-19 00:07:17 +0900

    chore

--- SnakeGame/Sources/ContentView.swift
+++ SnakeGame/Sources/ContentView.swift
@@ -1,35 +1,18 @@
 import SwiftUI
 
 public struct ContentView: View {
-
     @StateObject var model: ContentViewModel
     @State var lastMoveTime: TimeInterval = Date().timeIntervalSince1970
 
     public init(
         model: ContentViewModel
     ) {
-        self._model = .init(wrappedValue: model)
+        _model = .init(wrappedValue: model)
     }
 
     public var body: some View {
         TimelineView(.animation) { timelineContext in
-            let prevMoveTime = lastMoveTime
-            let now = timelineContext.date.timeIntervalSince1970
-            let diff = now - prevMoveTime
-
-            // 일단 1초에 한 번씩 움직이도록
-            if diff > 1 {
-                model.move()
-                lastMoveTime = now
-            }
-
-            return Canvas(
-                opaque: true,
-                colorMode: .linear,
-                rendersAsynchronously: false
-            ) { context, size in
-                drawCanvas(context: context, size: size)
-            }
+            timelineView(timelineContext)
         }
         .task {
             model.generateInitialSnake()
@@ -55,11 +38,32 @@ public struct ContentView: View {
         }
         .frame(width: 700, height: 700)
     }
+
+    private func timelineView(_ timelineContext: TimelineViewDefaultContext) -> some View {
+        let prevMoveTime = lastMoveTime
+        let now = timelineContext.date.timeIntervalSince1970
+        let diff = now - prevMoveTime
+
+        // 일단 1초에 한 번씩 움직이도록
+        if diff > 1 {
+            model.move()
+            lastMoveTime = now
+        }
+
+        return Canvas(
+            opaque: true,
+            colorMode: .linear,
+            rendersAsynchronously: false
+        ) { context, size in
+            drawCanvas(context: context, size: size)
+        }
+    }
+
     private func drawCanvas(context: GraphicsContext, size: CGSize) {
         var context = context
         context.opacity = 0.3
 
-        for i in (1..<Int(model.grid)) {
+        for i in 1 ..< Int(model.grid) {
             let y = size.height / model.grid * Double(i)
 
             let horizontalLinePath = Path { path in
@@ -82,7 +86,8 @@ public struct ContentView: View {
                 x: size.width / model.grid * square.x,
                 y: size.height / model.grid * square.y,
                 width: size.width / model.grid,
-                height: size.height / model.grid)
+                height: size.height / model.grid
+            )
             context.fill(Rectangle().path(in: rect), with: .color(.red))
         }
 
@@ -91,7 +96,8 @@ public struct ContentView: View {
                 x: size.width / model.grid * food.x,
                 y: size.height / model.grid * food.y,
                 width: size.width / model.grid,
-                height: size.height / model.grid)
+                height: size.height / model.grid
+            )
             context.fill(Circle().path(in: rect), with: .color(.yellow))
         }
     }
