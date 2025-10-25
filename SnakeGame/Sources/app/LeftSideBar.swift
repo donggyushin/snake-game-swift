0b4ec1cced0eed2082fcf9ef166561b5203f7157
신동규 <donggyu9410@gmail.com>
2025-10-25 21:18:59 +0900

    open new window

--- SnakeGame/Sources/app/LeftSideBar.swift
+++ SnakeGame/Sources/app/LeftSideBar.swift
@@ -4,8 +4,7 @@ struct LeftSideBar: View {
     @ObservedObject var parentModel: ContentViewModel
     @StateObject var model: LeftSideBarModel
     @State var showSaveGameRecordAlert = false
-    @State var showNicknameForm = false
-    @State var nickname = ""
+    @Environment(\.openWindow) var openWindow
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -34,32 +33,8 @@ struct LeftSideBar: View {
                 Task {
                     // Action to perform when OK is tapped
                     showSaveGameRecordAlert = false // Dismiss the alert explicitly if needed, though often automatic
-                    nickname = ""
                     try await Task.sleep(for: .seconds(0.7))
-                    withAnimation {
-                        showNicknameForm = true
-                    }
-                }
-            }
-        }
-        .overlay {
-            if showNicknameForm {
-                VStack(alignment: .leading, spacing: 30) {
-                    TextField("Nickname", text: $nickname)
-
-                    Button("Save") {
-                        guard !nickname.isEmpty else { return }
-                        model.saveGame(
-                            value: parentModel.currentScore,
-                            createdAt: Date(),
-                            nickname: nickname
-                        )
-                        nickname = ""
-                    }
-                }
-                .padding(20)
-                .background {
-                    RoundedRectangle(cornerRadius: 20)
+                    openWindow.callAsFunction(id: "nickname-form")
                 }
             }
         }
--- SnakeGame/Sources/app/SnakeGameApp.swift
+++ SnakeGame/Sources/app/SnakeGameApp.swift
@@ -2,9 +2,27 @@ import SwiftUI
 
 @main
 struct SnakeGameApp: App {
+    @State var nickname = ""
+
     var body: some Scene {
         WindowGroup {
             ContentView(model: .init(grid: 32))
+        }
+
+        // Define a new window with an identifier and content
+        Window("Nickname Form", id: "nickname-form") {
+            VStack(alignment: .leading, spacing: 30) {
+                TextField("Nickname", text: $nickname)
+
+                Button("Save") {
+                    guard !nickname.isEmpty else { return }
+                    print("should pass event to LeftSideBar")
+                }
+            }
+            .padding(20)
+            .onAppear {
+                nickname = ""
+            }
         }
     }
 }
