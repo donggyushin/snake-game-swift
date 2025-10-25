f7149156ed9086536be97034f2ade07d1572e9d8
신동규 <donggyu9410@gmail.com>
2025-10-25 21:09:46 +0900

    show save game alert

--- SnakeGame/Sources/app/LeftSideBar.swift
+++ SnakeGame/Sources/app/LeftSideBar.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct LeftSideBar: View {
     @ObservedObject var parentModel: ContentViewModel
     @StateObject var model: LeftSideBarModel
+    @State var showSaveGameRecordAlert = false
+    @State var showNicknameForm = false
+    @State var nickname = ""
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -12,6 +15,7 @@ struct LeftSideBar: View {
             if parentModel.isGameOver {
                 Button("Restart") {
                     parentModel.generateInitialSnake()
+                    model.canSaveGame = true
                 }
 
                 Button("Save Record") {
@@ -21,6 +25,43 @@ struct LeftSideBar: View {
         }
         .onAppear {
             model.fetchScores()
+        }
+        .alert("Save game record?", isPresented: $showSaveGameRecordAlert) {
+            Button("Cancel", role: .cancel) {
+                showSaveGameRecordAlert = false
+            }
+            Button("OK") {
+                Task {
+                    // Action to perform when OK is tapped
+                    showSaveGameRecordAlert = false // Dismiss the alert explicitly if needed, though often automatic
+                    nickname = ""
+                    try await Task.sleep(for: .seconds(0.7))
+                    withAnimation {
+                        showNicknameForm = true
+                    }
+                }
+            }
+        }
+        .overlay {
+            if showNicknameForm {
+                VStack(alignment: .leading, spacing: 30) {
+                    TextField("Nickname", text: $nickname)
+
+                    Button("Save") {
+                        guard !nickname.isEmpty else { return }
+                        model.saveGame(
+                            value: parentModel.currentScore,
+                            createdAt: Date(),
+                            nickname: nickname
+                        )
+                        nickname = ""
+                    }
+                }
+                .padding(20)
+                .background {
+                    RoundedRectangle(cornerRadius: 20)
+                }
+            }
         }
     }
 }
--- SnakeGame/Sources/app/LeftSideBarModel.swift
+++ SnakeGame/Sources/app/LeftSideBarModel.swift
@@ -1,11 +1,23 @@
 import Combine
+import Foundation
 
 final class LeftSideBarModel: ObservableObject {
     @Injected(\.scoreRepository) var scoreRepository
 
     @Published var scores: [Score] = []
+    @Published var canSaveGame = true
 
     @MainActor func fetchScores() {
         scores = scoreRepository.get()
+    }
+
+    @MainActor func saveGame(value: Int, createdAt: Date, nickname: String) {
+        canSaveGame = false
+
+        let id = UUID().uuidString
+
+        let score = Score(id: id, value: value, createdAt: createdAt, nickname: nickname)
+
+        scoreRepository.post(score)
     }
 }
